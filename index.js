1a15535fd8836a209907936deb947ca3238724af
Weenix Star <60514231+weenix1@users.noreply.github.com>
2021-09-15 17:56:08 +0200

    added an api and tried fetching data from the api

---
+++ index.js
@@ -0,0 +1,164 @@
+const loadImage = () => {
+  fetch("https://api.pexels.com/v1/search?query=your-query", {
+    method: "GET",
+    headers: {
+      Authorization: "563492ad6f91700001000001aaaca724bb564b948d088b4b473f9bb6",
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+
+    .then((body) => {
+      // DOM MANIPULATION
+      console.log(body);
+
+      const row = document.querySelector(".album .row");
+      row.innerHTML = "";
+
+      const array = body.photos;
+      for (let i = 0; i < array.length; i++) {
+        const obj = array[i];
+
+        const col = document.createElement("div");
+        col.className = "col-md-4";
+
+        col.innerHTML = `
+
+        
+        <div class="card mb-4 shadow-sm">
+        
+        
+        <img src="${obj.src.original}" class="img-fluid h-100 w-100" alt="...">
+       
+      
+
+          <div class="card-body">
+
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <div
+              class="d-flex justify-content-between align-items-center"
+            >
+              <div class="btn-group">
+                <button
+                  type="button"
+                  class="btn btn-sm btn-outline-secondary"
+                >
+                  View
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-sm btn-outline-secondary" 
+                  onclick="deleteCard(this.closest('.col-md-4'))"
+                >
+                  Hide
+                </button>
+              </div>
+              <small class="text-muted">9 mins</small>
+            </div>
+          </div>
+       
+      </div>
+                `;
+
+        row.appendChild(col);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+const loadSecondImg = () => {
+  fetch("https://api.pexels.com/v1/search?query=your-secondary-query", {
+    method: "GET",
+    headers: {
+      Authorization: "563492ad6f91700001000001aaaca724bb564b948d088b4b473f9bb6",
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+
+    .then((body) => {
+      // DOM MANIPULATION
+      console.log(body);
+
+      const row = document.querySelector(".album .row");
+
+      row.innerHTML = "";
+
+      const array = body.photos;
+      for (let i = 0; i < array.length; i++) {
+        const obj = array[i];
+
+        // const col = document.createElement("div");
+        // col.className = "col";
+
+        // col.innerHTML = `
+        // `;
+
+        // row.appendChild(col);
+
+        row.innerHTML += `
+        <div  class="col-md-4" 
+        >
+        <div class="card mb-4 shadow-sm">
+        
+        
+        <img src="${obj.src.original}" class="img-fluid h-100 w-100" alt="...">
+       
+       
+
+          <div class="card-body">
+
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <div
+              class="d-flex justify-content-between align-items-center"
+            >
+              <div class="btn-group">
+                <button
+                  type="button"
+                  class="btn btn-sm btn-outline-secondary"
+                >
+                  View
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-sm btn-outline-secondary" 
+                  onclick="deleteCard(this.closest('.col-md-4'))"
+
+                >
+                  Hide
+                </button>
+              </div>
+              <small class="text-muted">9 mins</small>
+            </div>
+          </div>
+        </div>
+      </div>
+        `;
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+function deleteCard(element) {
+  console.log(element);
+  element.remove();
+  // for (let card of document.querySelectorAll(".card")) {
+  //   card.remove();
+  // }
+}
